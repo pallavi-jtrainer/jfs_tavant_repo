87aacff32d3aa08886b473eeb714e4436753a044
Pallavi Prasad <pallavip06@gmail.com>
2025-02-20 15:45:49 +0530

    hibernate day1

---
+++ workspace_core_java/HibernateHelloWorld/src/main/java/com/tavant/sample/HibernateHelloWorld/utils/HibernateUtils.java
@@ -0,0 +1,39 @@
+package com.tavant.sample.HibernateHelloWorld.utils;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class HibernateUtils {
+	
+	private static SessionFactory sessionFactory = buildSessionFactory();
+	
+	private static SessionFactory buildSessionFactory() {
+		try {
+			if(sessionFactory == null) {
+				StandardServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+							.configure("hibernate.cfg.xml").build();
+				
+				Metadata metadata = new MetadataSources(serviceRegistry)
+						.getMetadataBuilder()
+						.build();
+				
+				sessionFactory = metadata.getSessionFactoryBuilder().build();
+			}
+		} catch(Throwable ex) {
+			System.out.println(ex.getMessage());
+		}
+		
+		return sessionFactory;
+	}
+	
+	public static SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+	
+	public static void shutdown() {
+		getSessionFactory().close();
+	}
+}
