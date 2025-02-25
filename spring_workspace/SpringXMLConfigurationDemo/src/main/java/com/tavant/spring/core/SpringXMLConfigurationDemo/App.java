fc201290bf6032a0ec88d9f3592fa13130d6546f
PallaviPrasad <61450900+pallavi-jtrainer@users.noreply.github.com>
2025-02-25 10:21:23 +0530

    java config in spring

--- spring_workspace/SpringXMLConfigurationDemo/src/main/java/com/tavant/spring/core/SpringXMLConfigurationDemo/App.java
+++ spring_workspace/SpringXMLConfigurationDemo/src/main/java/com/tavant/spring/core/SpringXMLConfigurationDemo/App.java
@@ -1,8 +1,10 @@
 package com.tavant.spring.core.SpringXMLConfigurationDemo;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.tavant.spring.core.SpringXMLConfigurationDemo.config.AppConfig;
 import com.tavant.spring.core.SpringXMLConfigurationDemo.entity.Course;
 import com.tavant.spring.core.SpringXMLConfigurationDemo.entity.HelloWorld;
 import com.tavant.spring.core.SpringXMLConfigurationDemo.entity.Student;
@@ -12,10 +14,19 @@ import com.tavant.spring.core.SpringXMLConfigurationDemo.entity.Student;
  */
 public class App {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-        
-        HelloWorld obj = context.getBean("demo", HelloWorld.class);
-        System.out.println(obj.getMessage());
+    	AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
+    	
+    	Student student = ctx.getBean(Student.class, "s");
+    	System.out.println(student.getStudentId() + " " + student.getStudentName()
+    			+ " " + student.getCourse().getCourseName());
+    	
+//    	HelloWorld hi = ctx.getBean(HelloWorld.class, "obj");
+//    	System.out.println(hi.getMessage());
+    	
+//        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+//        
+//        HelloWorld obj = context.getBean("demo", HelloWorld.class);
+//        System.out.println(obj.getMessage());
 
 //        Student s = context.getBean("student", Student.class);
 //        
