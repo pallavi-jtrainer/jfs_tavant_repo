c60820343bf26b65e5bfa6243af53753a5f9cadc
PallaviPrasad <61450900+pallavi-jtrainer@users.noreply.github.com>
2025-03-14 16:52:57 +0530

    react-bootstrap validation

--- react codes/posts-react-frontend/src/App.js
+++ react codes/posts-react-frontend/src/App.js
@@ -1,14 +1,37 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Register from './components/users/Register';
 import Login from './components/users/Login';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div >
-     {/* <Register /> */}
-     <Login/>
-    </div>
+    <BrowserRouter>
+      <div>
+        <nav className='navbar navbar-light bg-light'>
+          <div className="container-fluid navStyle">
+            <a className="navbar-brand" href='#'>Posts</a>
+            <ul className="navbar-nav" style={{display: 'ruby'}}>
+              <li className="nav-item">
+                <Link to="/register" className='nav-link'>Register</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/login" className='nav-link'>Login</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+
+        <Routes>
+          <Route path="/register" element={<Register/>}/>
+          <Route path="/login" element={<Login/>}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
+    // <div >
+    //  {/* <Register /> */}
+    //  <Login/>
+    // </div>
   );
 }
 
--- react codes/posts-react-frontend/src/components/users/Login.jsx
+++ react codes/posts-react-frontend/src/components/users/Login.jsx
@@ -1,19 +1,35 @@
 import '../../styles/Login.css';
+import { useState } from 'react';
 import { Container, Row, Col, Form, Button} from "react-bootstrap";
 
 export default function Login() {
+    const [validated, setValidated] = useState(false);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+            event.stopPropagation();
+        }
+
+        setValidated(true);
+    }
+
     return (
         <Container>
             <Row className="justify-content-md-center mt-5">
                 <Col xs={12} md={6}>
                     <h2 className="text-center mb-4 login-title">Login</h2>
-                    <Form>
+                    <Form noValidate validated={validated} onSubmit={handleSubmit}>
                         <Form.Group className="mb-3" controlId="formBasicEmail">
                             <Form.Label>Email/ Username</Form.Label>
                             <Form.Control
                                 type="email"
                                 placeholder="Enter email or username"
                             />
+                            <Form.Control.Feedback type="invalid">
+                                Please enter Username or Email.
+                            </Form.Control.Feedback>
                         </Form.Group>
 
                         <Form.Group className="mb-3" controlId="formBasicPassword">
@@ -22,6 +38,9 @@ export default function Login() {
                                 type="password"
                                 placeholder="Password"
                             />
+                            <Form.Control.Feedback type="invalid">
+                                Please enter a valid password.
+                            </Form.Control.Feedback>
                         </Form.Group>
 
                         <Button variant="primary" type="submit" className="w-100">
